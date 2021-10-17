7e3d2fc9dc6dfe7967f43767a0de0bf2a5af9cfd
ShockVeiw <fedorovleonid23@gmail.com>
2021-10-17 16:12:56 +0300

    example-01: implement event-bus

---
+++ example-01/event-bus/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+const PORT = 5002;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/events', async (req, res) => {
+  const { type, payload } = req.body;
+
+  axios.post('http://localhost:5000', { type, payload }).catch(err => console.log(err.message));
+  axios.post('http://localhost:5001', { type, payload }).catch(err => console.log(err.message));
+
+  res.end();
+});
+
+app.listen(PORT, () => {
+  console.log(`listening port: ${PORT}`);
+});
